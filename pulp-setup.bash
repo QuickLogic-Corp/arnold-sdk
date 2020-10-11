5c0862dc86c9252bb4a6bcedcffad10140473cde
“tim <saxe@quicklogic.com>
2020-10-11 15:02:12 -0700

    added runtime environment setup

---
+++ pulp-setup.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#
+# setup environment to run pulp tools
+#
+# you should 'source' this file before attempting to run any pulp tools
+#
+echo "setup pulp environment"
+pushd pulp-builder
+export PULP_RISCV_GCC_TOOLCHAIN=/opt/riscv
+source configs/arnold.sh
+source configs/board.sh
+source sdk-setup.sh
+export PULPRT_HOME=$PWD/pulp-rules
