5412ba3a3454cf174e2c9339cb73e2da79fd696d
“tim <saxe@quicklogic.com>
2020-10-15 20:52:00 -0700

    various updates

---
+++ patches/patch.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+#######################################################
+#
+# patch any files in the pulp install
+#
+#######################################################
+
+# fll-v1.c needs updating
+cp fll-v1.c ../pulp-builder/pulp-rt/kernel
