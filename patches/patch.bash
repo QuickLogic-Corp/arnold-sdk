7d819ccabab02313c7cd3329165a4c1e67c0faad
Greg Martin <gmartin@quicklogic.com>
2020-11-09 08:15:09 -0800

    Fixed bad path in patches.bash

--- patches/patch.bash
+++ patches/patch.bash
@@ -7,7 +7,7 @@
 
 # fll-v1.c needs updating
 echo "in patch.bash"
-cp bridge-makefile ../pulp-builder/pulp-rt/pulp-debug-bridge/Makefile
+cp bridge-makefile ../pulp-builder/pulp-debug-bridge/Makefile
 cp fll-v1.c ../pulp-builder/pulp-rt/kernel/
 cp bridge.c ../pulp-builder/pulp-rt/kernel/
 cp rt_freq.h ../pulp-builder/pulp-rt/include/rt/
