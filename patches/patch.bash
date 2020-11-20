11275f68353d98c7293cc969db3450ce837cd3cc
Greg Martin <gmartin@quicklogic.com>
2020-11-19 18:52:03 -0800

    Updates to add fpga based i2c changes to camera drivers for person-detect example

--- patches/patch.bash
+++ patches/patch.bash
@@ -11,4 +11,6 @@ cp bridge-makefile ../pulp-builder/pulp-debug-bridge/Makefile
 cp fll-v1.c ../pulp-builder/pulp-rt/kernel/
 cp bridge.c ../pulp-builder/pulp-rt/kernel/
 cp rt_freq.h ../pulp-builder/pulp-rt/include/rt/
+cp camera.c ../pulp-builder/pulp-rt/drivers_deprecated/camera/
+cp himax.c ../pulp-builder/pulp-rt/drivers_deprecated/camera/
 cp implem.h ../pulp-builder/pulp-rt/include/pmsis/implem/ || (echo "copy failed"; exit 1)
